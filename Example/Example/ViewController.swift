b2b36430ec313c6603060f7c4b8523418f8b01b4
Adrian Bobrowski <adrian071993@gmail.com>
2017-08-27 21:15:06 +0200

    Created a basic example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Adrian Bobrowski on 23.08.2017.
+//  Copyright © 2017 Adrian Bobrowski (Decybel07), adrian071993@gmail.com. All rights reserved.
+//
+
+import UIKit
+import SwiftyKeyboardObserver
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
